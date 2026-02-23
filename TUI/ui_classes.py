8596d3e5502309ad96fcafd9325c42032620352d
binilbp <biniloffcl@gmail.com>
2026-02-23 19:26:00 +0000

    Add TerminalScreen and StatusBar classes to ui_classes

--- TUI/ui_classes.py
+++ TUI/ui_classes.py
@@ -2,8 +2,8 @@
 
 
 from textual.app import ComposeResult
-from textual.widgets import Label
-from textual.containers import Container
+from textual.widgets import Label, RichLog, LoadingIndicator
+from textual.containers import Container, Horizontal
 
 
 
@@ -17,3 +17,33 @@ class ASCIname(Container):
             art = "LINQUIX"
         yield Label(art, id="ascii")
 
+
+
+class TerminalScreen(Container):
+    def compose(self) -> ComposeResult:
+        terminal_screen = RichLog(
+            id="terminal_screen",
+            auto_scroll = True, 
+            highlight=True, 
+            markup = True
+        )
+        # terminal_screen.border_title = "Terminal"
+        terminal_screen.can_focus = False
+        yield terminal_screen 
+
+    def on_mount(self) -> None:
+        self.query_one("#terminal_screen", RichLog).write("  [blue] [/]  How can I help you today?\n", animate = True)
+
+
+
+class StatusBar(Horizontal):
+    def compose(self) -> ComposeResult:
+        yield LoadingIndicator(id="loading-bar")
+        status_line = RichLog(
+                id="status-line",
+                markup = True
+        )
+        yield status_line
+
+    def on_mount(self) -> None:
+        self.query_one("#status-line", RichLog).write("Running as [red]NON-ROOT[/] user")
